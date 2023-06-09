2d2723467f4ecaf6551367f039957e7f9e0fe2c0
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-06-09 20:21:11 +0530

    chore: enable syntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,33 @@ import starlight from "@astrojs/starlight";
 // https://astro.build/config
 export default defineConfig({
   site: "https://tmfm.buildwithhussain.dev",
+  markdown: {
+    shikiConfig: {
+      theme: "material-theme-palenight",
+      wrap: true,
+    }
+  },
   integrations: [
     starlight({
-      title: "The Missing Frappe Manual",
+      title: "Missing Frappe Manual",
       social: {
         github: "https://github.com/NagariaHussain",
         twitter: "https://twitter.com/NagariaHussain",
       },
       sidebar: [
         {
-          label: "Guides",
+          label: "FSchool Handouts",
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", link: "/guides/example/" },
+            { label: "Day 0", link: "/handouts/day-0" },
+            { label: "Day 1", link: "/handouts/day-1" },
+            { label: "Day 2", link: "/handouts/day-2" },
+            { label: "Day 3", link: "/handouts/day-3" },
           ],
         },
         {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
+          label: "No Code",
+          autogenerate: { directory: "no-code" },
         },
       ],
     }),
