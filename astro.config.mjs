fb272458c8cd7f1d7bbabb284c7b5dd5fcdc57e6
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-07-03 09:26:48 +0530

    chore: day 4 recording link etc.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,58 +7,81 @@ export default defineConfig({
   markdown: {
     shikiConfig: {
       theme: "github-dark",
-      wrap: true
-    }
+      wrap: true,
+    },
   },
-  integrations: [starlight({
-    title: "Missing Frappe Manual",
-    editLink: {
-      baseUrl: "https://github.com/NagariaHussain/the-missing-frappe-manual/edit/main/"
-    },
-    social: {
-      github: "https://github.com/NagariaHussain",
-      twitter: "https://twitter.com/NagariaHussain"
-    },
-    sidebar: [{
-      label: "Introduction",
-      items: [{
-        label: "What is Frappe Framework?",
-        link: "/introduction/ff-definition"
-      }]
-    }, {
-      label: "Installation",
-      items: [{
-        label: "Easy Install With Docker",
-        link: "/installation/docker-easy-install"
-      }]
-    }, {
-      label: "FSchool Handouts",
-      items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: "Day 0",
-        link: "/handouts/day-0"
-      }, {
-        label: "Day 1",
-        link: "/handouts/day-1"
-      }, {
-        label: "Day 2",
-        link: "/handouts/day-2"
-      }, {
-        label: "Day 3",
-        link: "/handouts/day-3"
-      }]
-    }, {
-      label: "Comparison",
-      items: [{
-        label: "Frappe Framework vs Django",
-        link: "/comparison/ff-vs-django"
-      }]
-    }, {
-      label: "No Code",
-      autogenerate: {
-        directory: "no-code"
-      }
-    }]
-  })]
+  integrations: [
+    starlight({
+      title: "Missing Frappe Manual",
+      editLink: {
+        baseUrl:
+          "https://github.com/NagariaHussain/the-missing-frappe-manual/edit/main/",
+      },
+      social: {
+        github: "https://github.com/NagariaHussain",
+        twitter: "https://twitter.com/NagariaHussain",
+      },
+      sidebar: [
+        {
+          label: "Introduction",
+          items: [
+            {
+              label: "What is Frappe Framework?",
+              link: "/introduction/ff-definition",
+            },
+          ],
+        },
+        {
+          label: "Installation",
+          items: [
+            {
+              label: "Easy Install With Docker",
+              link: "/installation/docker-easy-install",
+            },
+          ],
+        },
+        {
+          label: "FSchool Handouts",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: "Day 0",
+              link: "/handouts/day-0",
+            },
+            {
+              label: "Day 1",
+              link: "/handouts/day-1",
+            },
+            {
+              label: "Day 2",
+              link: "/handouts/day-2",
+            },
+            {
+              label: "Day 3",
+              link: "/handouts/day-3",
+            },
+            {
+              label: "Day 4",
+              link: "/handouts/day-4",
+            },
+          ],
+        },
+        {
+          label: "Comparison",
+          items: [
+            {
+              label: "Frappe Framework vs Django",
+              link: "/comparison/ff-vs-django",
+            },
+          ],
+        },
+        {
+          label: "No Code",
+          autogenerate: {
+            directory: "no-code",
+          },
+        },
+      ],
+    }),
+  ],
 });
---
+++ src/content/docs/handouts/day-4.md
@@ -0,0 +1,32 @@
+---
+title: Day 4
+description: Optional Advanced Stuff
+---
+
+## Video Recording
+
+The video recording of the 4th day (BONUS!) of training session can be found [here](https://drive.google.com/file/d/1fhdfMpaupo3CdB3G2bTiyCP8QcNh0-JA/view?usp=drive_link).
+
+Topic covered in the above training session:
+
+* Caching
+* Queueing
+* Real time (Socket.IO)
+* Debugging & Profiling
+* Unit Tests
+
+**We also investigated a bug, fixed it and raised a PR, live!**
+
+## Reading
+
+* [Official Docs on Debugging](https://frappeframework.com/docs/v14/user/en/debugging)
+
+* [Official Docs on Realtime](https://frappeframework.com/docs/v14/user/en/api/realtime)
+
+* [Official Guide on Caching](https://frappeframework.com/docs/v14/user/en/guides/caching)
+
+* [The Python Debugger: `pdb` Documentation](https://docs.python.org/3/library/pdb.html)
+
+## Viewing
+
+* [Ankush's talk from ERPNext Conference 2022](https://youtu.be/KTWwvHnjF5w)
