b440faf6bed7ca448dfe7d48477f6a142cd80c69
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-12-16 14:20:33 +0530

    chore: use local platform URL in dev

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,7 @@
-export const PLATFORM_BASE_URL = "https://platform.buildwithhussain.dev";
+let BASE_URL = "https://platform.buildwithhussain.dev/";
+
+if (process.env.NODE_ENV === "development") {
+  BASE_URL = "http://hcloud.localhost:8000/";
+}
+
+export const PLATFORM_BASE_URL = BASE_URL;
